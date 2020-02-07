ca1d705247ba4d3851b0d5d48448dc38064ad9b4
1C - Thiện Nguyễn <pandarencute96@gmail.com>
2020-02-07 08:40:33 +0700

    add ex02_escape_char

--- Step01_SteppingIntoTheWorldOfPython/ex01_printing.py
+++ Step01_SteppingIntoTheWorldOfPython/ex01_printing.py
@@ -1,7 +1,26 @@
-#Region syntax_and_parameters
+# Region syntax_and_parameters
 
-print('Hello World')
-print(6*7)
+# syntax
+# print(object(s), separator=separator, end=end, file=file, flush=flush)
 
-#EndRegion
+# parameter values
+# -----------------------------------------#
+# Parameter               | Description                                                                                           |
+# --------------------------------------------------------------------------------------------------------------------------------|
+# objects                 | Any object, and as many as you like. Will be converted to string before printed                       |
+# sep = 'separator'       | Optional. Specify how to separate the objects, if there is more than one. Default is ' '              |
+# end = 'end'             | Optional. Specify what to print at the end. Default is '\n' (line feed)                               |
+# file                    | Optional. An object with a write method. Default is sys.stdout                                        |
+# flush                   | Optional. A Boolean, specifying if the output is flushed (True) or buffered (False). Default is False |
 
+# EndRegion
+
+# print more than 1 objects
+print('Hello World', 'how are you?')
+
+# print a tuple
+tuple = ("a", "b", "c")
+print(tuple)
+
+# print two messagesm and specify the separator:
+print("Hello", "how are you?", sep=", ")
---
+++ Step01_SteppingIntoTheWorldOfPython/ex02_escape_char.py
@@ -0,0 +1,14 @@
+# split string
+splitString = "This string has been \nsplit over \nseveral \nlines"
+print(splitString)
+
+# split tab
+splitTab = "1 \t2 \t3 \t4 \t5"
+print(splitTab)
+
+# examples of multiple way to escape char
+print('Ich sage "I\'ve finished my homework".')
+# or
+print("Ich sage \'I've finished my homework\'.")
+# or
+print("""Ich sage "I've finished my homework".""")
