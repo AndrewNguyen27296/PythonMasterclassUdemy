fc85040d9b177d176a5c4c9bfbeadc300d8d09a3
XinKaChu <pandarencute96@gmail.com>
2020-02-09 15:18:00 +0700

    Add variables and types file

---
+++ Step01_SteppingIntoTheWorldOfPython/ex03_variables_types.py
@@ -0,0 +1,11 @@
+# String example
+name = input('Please enter your name: ')
+print("Greeting " + name)
+
+# Number example
+age = 24
+print(age)
+
+# Type example
+print(type(name))
+print(type(age))
