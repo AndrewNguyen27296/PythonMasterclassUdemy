96a8cb6f9af229851b2e4546bbe57d83f25aaebd
XinKaChu <pandarencute96@gmail.com>
2020-02-24 00:20:12 +0700

    Add new file

---
+++ Step01_SteppingIntoTheWorldOfPython/ex05_str_data_type.py
@@ -0,0 +1,21 @@
+Sample = "Norwegian Blue"
+
+# Index
+print(Sample[3])
+print(Sample[4])
+print(Sample[9])
+print(Sample[3])
+print(Sample[6])
+print(Sample[8])
+
+# Negative number
+print(Sample[-1])
+
+# Slicing
+print(Sample[1:6])
+
+# Slice in step
+# System will print every 2 indexes
+print(Sample[1:10:2]) #owga
+
+
