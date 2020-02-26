9e5f1cdbfa4498ed7a13fd3c55307f635c05c878
1C - Thiện Nguyễn <pandarencute96@gmail.com>
2020-02-26 11:32:30 +0700

    Add slice challenge

--- Step01_SteppingIntoTheWorldOfPython/ex05_str_data_type.py
+++ Step01_SteppingIntoTheWorldOfPython/ex05_str_data_type.py
@@ -18,4 +18,14 @@ print(Sample[1:6])
 # System will print every 2 indexes
 print(Sample[1:10:2]) #owga
 
+# Slicing backward
+print(Sample[::-1]) #eulB naigewroN
 
+# Challenge slice
+letters = "abcdefghijklmnopqrstuvwxyz"
+# Create a slice that produces the characters qpo
+print(letters[16:13:-1])
+# Slice the string to produce edcba
+print(letters[4::-1])
+# Slice the string to produce last 8 character, in reverse order
+print(letters[-1:-9:-1])
