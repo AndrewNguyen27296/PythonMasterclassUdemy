dbca1a92ed92b296b20f27aa0ef219f16b821934
XinKaChu <pandarencute96@gmail.com>
2020-03-02 00:25:02 +0700

    Add string file

--- Step01_SteppingIntoTheWorldOfPython/ex05_str_data_type.py
+++ Step01_SteppingIntoTheWorldOfPython/ex05_str_data_type.py
@@ -18,4 +18,17 @@ print(Sample[1:6])
 # System will print every 2 indexes
 print(Sample[1:10:2]) #owga
 
+# Region String Replacement Fields
+age = 24
+print("My age is " + str(age) + " years")
+print("My age is {0} years".format(age))
+# EndRegion
+
+# Region Formatting
+for i in range(1, 13):
+    # 2,3,4 is the space, left
+    print("No. {0:2} squared is {1:3} and cubed is {2:4}".format(i, i**2, i**3))
+    # left, mid, center
+    print("No. {0:2} squared is {1:<3} and cubed is {2:>4}".format(i, i ** 2, i ** 3))
+# EndRegion
 
