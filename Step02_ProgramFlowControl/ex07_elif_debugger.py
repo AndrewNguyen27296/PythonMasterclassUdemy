4622bb7ef44cd6db0581f59a5046a8ef3992badc
XinKaChu <pandarencute96@gmail.com>
2020-04-03 13:34:49 +0700

    Add files

--- Step02_ProgramFlowControl/ex07_elif_debugger.py
+++ Step02_ProgramFlowControl/ex07_elif_debugger.py
@@ -4,19 +4,45 @@ print("Please guess number between 1 and 10: ")
 
 guess = int(input())
 
-if guess < answer:
-    print("Please guess higher")
+if guess != answer:
+    if guess < answer:
+        print("Please guess higher")
+    else:
+        print("Please guess lower")
     guess = int(input())
     if guess == answer:
-        print("Well done, you guessed it correct")
+        print("Well done, you guessed it correctly")
     else:
-        print("Sorry, you lose.")
-elif guess > answer:
-    print("Please guess lower")
-    guess = int(input())
-    if guess == answer:
-        print("Well done, you guessed it correct")
-    else:
-        print("Sorry, you lose.")
+        print("Sorry, you lose")
 else:
+    print("You got it in the first time")
+
+# challenge
+if guess == answer:
     print("You got it first time")
+else:
+    if guess < answer:
+        print("Please guess higher")
+    else:
+        print("Please guesss lower")
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guessed it correctly")
+    else:
+        print("Sorry, you lose")
+# if guess < answer:
+#     print("Please guess higher")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it correct")
+#     else:
+#         print("Sorry, you lose.")
+# elif guess > answer:
+#     print("Please guess lower")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it correct")
+#     else:
+#         print("Sorry, you lose.")
+# else:
+#     print("You got it first time")
---
+++ Step02_ProgramFlowControl/ex08_conditions.py
@@ -0,0 +1,13 @@
+age = int(input("How old are you? "))
+
+if 16 <= age <= 65:
+    print("Have a good day at work")
+else:
+    print("Enjoy your free time")
+
+print("-" * 50)
+
+if 16 > age > 65:
+    print("Enjoy your free time")
+else:
+    print("Have a good day at work")
