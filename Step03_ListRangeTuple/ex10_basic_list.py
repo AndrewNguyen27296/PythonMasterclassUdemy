e7d990a99f3f288e192a410d269de00a6fc65f62
XinKaChu <pandarencute96@gmail.com>
2020-05-31 22:34:28 +0700

    Add files

---
+++ Step03_ListRangeTuple/ex10_basic_list.py
@@ -0,0 +1,13 @@
+parrot_list = ['non pinin', 'no more', 'a stiff']
+
+parrot_list.append('a Norwegian blue')
+
+for item in parrot_list:
+    print(item)
+
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7]
+
+numbers = even + odd
+numbers.sort()
+print(numbers)
